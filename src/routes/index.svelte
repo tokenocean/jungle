ece9f11b50a4da85939c42ee48e3952b355e0781
Esky33 <djescaba@icloud.com>
2022-01-26 10:15:10 +0000

    working env and login fix

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -262,6 +262,154 @@
   </div>
 </div>
 
+<!--
+  This example2 requires Tailwind CSS v2.0+ 
+  
+  This example2 requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+-->
+<div class="bg-white-100">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-2xl mx-auto py-16 sm:py-24 lg:py-32 lg:max-w-none">
+      <h2 class="text-2xl font-extrabold text-gray-900">Collections</h2>
+
+      <div class="mt-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6">
+        <div class="group relative">
+          <div class="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
+            <img src="https://images.unsplash.com/photo-1471341971476-ae15ff5dd4ea?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8c2VhcmNofDF8fFN0dWRpb3xlbnwwfHx8fDE2MzcxMTAyNDQ&ixlib=rb-1.2.1&q=80&w=2000" alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug." class="w-full h-full object-center object-cover">
+          </div>
+          <h3 class="mt-6 text-sm text-gray-500">
+            <a href="https://junglelab.net/music/">
+              <span class="absolute inset-0"></span>
+              Music
+            </a>
+          </h3>
+          <p class="text-base font-semibold text-gray-900">Swag Exclusive</p>
+        </div>
+
+        <div class="group relative">
+          <div class="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
+            <img src="https://junglelab.net/content/images/size/w960/2021/11/19A8303F-62D7-403E-A846-43894D70EEFD.jpeg" alt="Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant." class="w-full h-full object-center object-cover">
+          </div>
+          <h3 class="mt-6 text-sm text-gray-500">
+            <a href="https://junglelab.net/video/">
+              <span class="absolute inset-0"></span>
+              Video
+            </a>
+          </h3>
+          <p class="text-base font-semibold text-gray-900">Exclusive</p>
+        </div>
+
+        <div class="group relative">
+          <div class="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
+            <img src="https://junglelab.net/content/images/size/w960/2021/11/QmXJhqJX5sGgRwLua4PmUBpTtXnFpRFhTad37iSPrnoCDC-1.png" alt="Collection of four insulated travel bottles on wooden shelf." class="w-full h-full object-center object-cover">
+          </div>
+          <h3 class="mt-6 text-sm text-gray-500">
+            <a href="https://junglelab.net/art/">
+              <span class="absolute inset-0"></span>
+              Art
+            </a>
+          </h3>
+          <p class="text-base font-semibold text-gray-900">Exclusive</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- This is the information area -->
+<div class="py-12 bg-white">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="lg:text-center">
+      <h2 class="text-base text-black font-semibold tracking-wide uppercase">Liquid Digital Assets</h2>
+      <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        JungleLab.io
+      </p>
+      <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+        The Revolutionary urban lifestyle LNFT platform for artists, creatives and collectors.
+        <p>
+        <a
+        href="https://jungle.coinos.io/guide"
+        target="_blank">JungleLab User & Artist Guide Tutorial</a>
+      </p>
+    </div>
+
+    <div class="mt-10">
+      <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+        <div class="relative">
+          <dt>
+            <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-gray-500 text-white">
+              <!-- Heroicon name: outline/globe-alt -->
+              <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+              </svg>
+            </div>
+            <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Global Market</p>
+          </dt>
+          <dd class="mt-2 ml-16 text-base text-gray-500">
+            Jungle Lab is a revolutionary urban lifestyle NFT platform for artists, creatives and collectors to upload and transact rare digital assets including music, videos and art.
+          </dd>
+        </div>
+
+        <div class="relative">
+          <dt>
+            <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-gray-500 text-white">
+              <!-- Heroicon name: outline/scale -->
+              <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
+              </svg>
+            </div>
+            <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Scale Faster</p>
+          </dt>
+          <dd class="mt-2 ml-16 text-base text-gray-500">
+            Streamlining talent eCommerce portfolio and process - Manage all your digital products/assets in one place i.e. albums, mixtapes, singles, events, merchandise and exclusive artwork.
+
+          </dd>
+        </div>
+
+        <div class="relative">
+          <dt>
+            <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-gray-500 text-white">
+              <!-- Heroicon name: outline/lightning-bolt -->
+              <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+              </svg>
+            </div>
+            <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Liquid Network</p>
+          </dt>
+          <dd class="mt-2 ml-16 text-base text-gray-500">
+            We exist to support millions of artists and creatives with an eco-system that provides rights, ownership, and the opportunity to create multiple independent revenue streams.
+          </dd>
+        </div>
+
+        <div class="relative">
+          <dt>
+            <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-gray-500 text-white">
+              <!-- Heroicon name: outline/annotation -->
+              <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+              </svg>
+            </div>
+            <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Top Artists</p>
+          </dt>
+          <dd class="mt-2 ml-16 text-base text-gray-500">
+            Help you adopt exclusive community - Adopt special privileges for your fanbase that follows you on our platform. i.e. early ticket release, exclusive events backstage passes attached to digital assets.
+          </dd>
+        </div>
+      </dl>
+    </div>
+  </div>
+</div>
+
 <style>
   .header {
     width: 90%;
