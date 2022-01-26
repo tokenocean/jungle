ffcde686aa45383c14b4e215cdc4bc170dc005be
Esky33 <djescaba@icloud.com>
2022-01-26 03:26:55 +0000

    correct html fix index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,6 @@
 <script context="module">
   export async function load({ fetch }) {
     const props = await fetch(`/artworks/recent.json`).then((r) => r.json());
-
     return {
       props,
     };
@@ -19,22 +18,17 @@
   import branding from "$lib/branding";
   import { prefetch } from "$app/navigation";
   import { browser } from "$app/env";
-
   onMount(() => browser && prefetch("/market"));
-
   export let featured;
   export let recent;
   export let latest;
-
   let current = 0;
   $: artwork = featured && featured[current] && featured[current].artwork;
-
   let interval = setInterval(() => {
     if (!featured) return;
     current++;
     if (current >= featured.length) current = 0;
   }, 6000);
-
   onDestroy(() => clearInterval(interval));
 </script>
 
@@ -185,6 +179,22 @@
   </div>
 </div>
 <!-- This is the information area -->
+<div class="py-12 bg-white">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="lg:text-center">
+      <h2 class="text-base text-black font-semibold tracking-wide uppercase">Liquid Digital Assets</h2>
+      <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        JungleLab.io
+      </p>
+      <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+        The Revolutionary urban lifestyle LNFT platform for artists, creatives and collectors.
+        <p>
+        <a
+        href="https://jungle.coinos.io/guide"
+        target="_blank">JungleLab User & Artist Guide Tutorial</a>
+      </p>
+    </div>
+
     <div class="mt-10">
       <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
         <div class="relative">
@@ -257,12 +267,10 @@
     width: 90%;
     margin-top: 128px;
   }
-
   .header .primary-btn {
     width: 240px;
     margin: 0 auto;
   }
-
   .header h5 {
     font-size: 22px;
     line-height: 36px;
@@ -270,13 +278,11 @@
     margin-top: 24px;
     margin-bottom: 34px;
   }
-
   .secondary-header {
     height: 600px !important;
     width: 100%;
     object-fit: cover;
   }
-
   .blur-bg {
     display: flex;
     padding: 60px;
@@ -289,76 +295,60 @@
     width: 50%;
     width: fit-content;
   }
-
   .blur-bg h2 {
     color: white !important;
   }
-
   .blur-bg p {
     color: white !important;
     margin-top: 20px;
   }
-
   .container.more {
     display: flex;
     justify-content: center;
     margin: 0 auto;
     margin-top: 36px;
   }
-
   .more .secondary-btn {
     width: 180px;
   }
-
   .header-button {
     width: 200px;
     border: 1px solid;
     border-radius: 30px;
     padding: 0.7rem 1.5rem !important;
   }
-
   h3 {
     margin-bottom: 36px;
   }
-
   .marg-bottom {
     margin-bottom: 128px !important;
   }
-
   @media only screen and (max-width: 768px) {
     .header-container.marg-bottom {
       margin-bottom: 96px !important;
     }
-
     .header {
       margin-top: 64px;
     }
-
     h3 {
       margin-bottom: 32px;
     }
-
     .header h5 {
       margin-top: 24px;
       margin-bottom: 24px;
     }
-
     .header .primary-btn {
       width: 100%;
     }
-
     .secondary-header {
       height: 400px !important;
     }
-
     .container.more {
       margin-top: 48px;
     }
-
     .marg-bottom {
       margin-bottom: 96px !important;
     }
-
     .blur-bg {
       padding: 24px;
       width: 75%;
