b857747366e40e2dcde21ee0ecccb9e04968505e
Adam Soltys <asoltys@gmail.com>
2022-02-26 21:53:12 -0800

    remove analytics code

--- src/components/Head.svelte
+++ src/components/Head.svelte
@@ -31,35 +31,4 @@
   <meta property="og:url" content={host + $page.url.pathname} />
   <meta name="twitter:site" content={metadata.twitter.site} />
   <meta name="twitter:creator" content={metadata.twitter.creator} />
-
-  {#if !dev}
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script
-      async
-      src="https://www.googletagmanager.com/gtag/js?id=G-H25E4PKPEC"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag("js", new Date());
-      gtag("config", "G-H25E4PKPEC");
-    </script>
-
-    <script>
-      (function (h, o, t, j, a, r) {
-        h.hj =
-          h.hj ||
-          function () {
-            (h.hj.q = h.hj.q || []).push(arguments);
-          };
-        h._hjSettings = { hjid: 2706575, hjsv: 6 };
-        a = o.getElementsByTagName("head")[0];
-        r = o.createElement("script");
-        r.async = 1;
-        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-        a.appendChild(r);
-      })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
-    </script>
-  {/if}
 </svelte:head>
