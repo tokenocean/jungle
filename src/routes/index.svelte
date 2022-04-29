1f27dabd5bf9c4164bc37e9737ee2b1682a41256
Esky33 <djescaba@icloud.com>
2022-04-29 03:33:31 +0100

    update info

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -378,7 +378,9 @@
 <div class="bg-white-100">
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="max-w-2xl mx-auto py-16 sm:py-24 lg:py-32 lg:max-w-none">
-      <h2 class="text-2xl font-extrabold text-gray-900">Collections</h2>
+      <h2 class="text-2xl font-extrabold text-gray-900">
+        Rare Dank Collections
+      </h2>
 
       <div
         class="mt-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6"
