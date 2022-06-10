b686a89c3216f7c99750c581be0fd848ee7aa247
Adam Soltys <asoltys@gmail.com>
2022-06-09 23:44:48 -0700

    restore links

--- src/routes/a/[slug].svelte
+++ src/routes/a/[slug].svelte
@@ -326,6 +326,7 @@
       </div>
 
       <div class="flex flex-wrap justify-between text-left">
+        <a href={`/${artwork.artist.username}`}>
         <div class="flex mb-6">
           <Avatar user={artwork.artist} />
           <div class="ml-2 secondary-color">
@@ -333,8 +334,10 @@
             <div class="text-xs text-gray-600">Artist</div>
           </div>
         </div>
+        </a>
 
         {#if artwork.artist_id !== artwork.owner_id && artwork.held}
+        <a href={`/${artwork.owner.username}`}>
           <div class="flex mb-6 secondary-color">
             <Avatar user={artwork.owner} />
             <div class="ml-2">
@@ -342,6 +345,7 @@
               <div class="text-xs text-gray-600">Owner</div>
             </div>
           </div>
+          </a>
         {/if}
         {#if !artwork.held}
           <a href="https://bitcoin.org/bitcoin.pdf">
