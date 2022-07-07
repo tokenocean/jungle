ddd507ad1a6c630a6e7fcdd6736c448a36427c2d
Adrien Hopkins <ahopk127@my.yorku.ca>
2022-07-07 12:49:08 -0500

    fix svelte move to vite

--- svelte.config.js
+++ svelte.config.js
@@ -3,31 +3,11 @@ import tailwind from "tailwindcss";
 import autoprefixer from "autoprefixer";
 import postcss from "postcss-preset-env";
 import nesting from "postcss-nesting";
-import path from "path";
 import node from "@sveltejs/adapter-node";
-import shim from "@asoltys/vite-plugin-stream-shim";
 
 export default {
   kit: {
     adapter: node(),
-    vite: {
-      plugins: [shim()],
-      resolve: {
-        alias: {
-          $comp: path.resolve("src/components/index.js"),
-          $components: path.resolve("src/components"),
-          $queries: path.resolve("src/queries"),
-        },
-      },
-      server: {
-        proxy: {
-          "/api": {
-            target: "http://localhost:8091",
-            rewrite: (path) => path.replace(/^\/api/, ""),
-          },
-        },
-      },
-    },
   },
   preprocess: preprocess({
     postcss: {
---
+++ vite.config.js
@@ -0,0 +1,26 @@
+// vite.config.js
+import { sveltekit } from '@sveltejs/kit/vite';
+import shim from "@asoltys/vite-plugin-stream-shim";
+import path from "path";
+
+/** @type {import('vite').UserConfig} */
+const config = {
+  plugins: [sveltekit(), shim()],
+  resolve: {
+    alias: {
+      $comp: path.resolve("src/components/index.js"),
+      $components: path.resolve("src/components"),
+      $queries: path.resolve("src/queries"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8091",
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+};
+
+export default config;
