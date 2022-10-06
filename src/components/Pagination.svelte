a810ff5e4a3f4073eef1d4343f10102e80d8b729
John Barratt <johnnybarratt@gmail.com>
2022-10-06 14:47:18 -0700

    commit before remote push

--- src/components/Pagination.svelte
+++ src/components/Pagination.svelte
@@ -1,22 +1,29 @@
 <script>
   import { offset } from "$lib/store";
+  import { onMount } from 'svelte';
   import Fa from "svelte-fa";
   import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
   export let loadMore, total;
   let pageSize = 21;
   let current = 0;
+  let buttonLength = 5;
 
   $: pages = total > 0 ? [...Array(Math.ceil(total / pageSize)).keys()] : [];
-  
+  $: pagesLeft = pages.slice(current - 2, current)
+  $: pagesRight = pages.slice(current, current + 2)
+
+  console.log("pagesleft:", pagesLeft, "pagesRight:", pagesRight)
   let load = (page) => {
     current = page;
     $offset = page * pageSize;
     loadMore();
   };
+      
 </script>
 
 <div class="full-width flex bg-white p-4 mx-auto">
   <div class="mx-auto">
+    
     <div class="container flex mb-2">
       <i class="icon w-12 h-12" class:font-bold={current}
         on:click={() => load(0)}>
@@ -27,14 +34,20 @@
         on:click={() => load(current - 1)}>
         <Fa icon={faChevronLeft} />
       </i>
-      {#each pages as _, i}
-
+      {#each pages.slice(Math.max(current - 2, 0), Math.min(current +2, pages.length)) as _, i}
+        <button
+          class="rounded-full w-12 h-12"
+          class:font-bold={i === current}
+          on:click={() => load(i)}>{Math.max(current -2, i ) + 1}</button
+        >
+      {/each}
+      <!-- {#each pagesRight as _, i}
         <button
           class="rounded-full w-12 h-12"
           class:font-bold={i === current}
           on:click={() => load(i)}>{i + 1}</button
         >
-      {/each}
+      {/each} -->
       <i class="icon w-12 h-12" class:font-bold={current}
         on:click={() => load(current + 1)}>
         <Fa icon={faChevronRight} />
@@ -71,3 +84,4 @@
   }
 
 </style>
+
