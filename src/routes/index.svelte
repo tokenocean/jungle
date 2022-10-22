bcc2afb1394c99e8f26ae33b193bf0adcf8ad568
Esky33 <djescaba@icloud.com>
2022-10-22 20:33:45 +0000

    Freedom is a Must. Take Yours. update info

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -148,7 +148,7 @@
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="max-w-2xl mx-auto py-16 sm:py-24 lg:py-32 lg:max-w-none">
       <h2 class="text-2xl font-extrabold text-gray-900">
-        Rare Dank Collections
+        Freedom is a Must. Take Yours.
       </h2>
 
       <div
