e1f3e9261676f8ca45bc4b45db9b3422a51a2153
Esky33 <djescaba@icloud.com>
2022-10-22 21:44:08 +0000

    Liquid Bitcoin Lightning Network - text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -219,7 +219,7 @@
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="lg:text-center">
       <h2 class="text-base text-black font-semibold tracking-wide uppercase">
-        Liquid Bitcoin Lightning Digital Assets
+        Liquid Bitcoin Lightning Network
       </h2>
       <p
         class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl"
