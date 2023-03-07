619e5fdfb3ac4c1a4c5b1bfff52554216ed45693
Esky33 <djescaba@icloud.com>
2023-03-07 17:06:20 +0000

    update tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,42 +1,23 @@
 module.exports = {
-  mode: "jit",
-  content: ["./src/**/*.{js,svelte}"],
+  mode: 'jit',
+  content: [
+    './src/**/*.{js,svelte}',
+  ],
   theme: {
     screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
     },
-     extend: {
-      extend: {
-      'animation': {
-            'text':'text 8s ease infinite',
-        },
-        'keyframes': {
-            'text': {
-                '0%, 100%': {
-                   'background-size':'200% 200%',
-                    'background-position': 'left center'
-                },
-                '50%': {
-                   'background-size':'200% 200%',
-                    'background-position': 'right center'
-                }
-            },
-        }
-    },  
+    extend: {
       colors: {
         primary: "#FF8B00",
         secondary: "#CEDC21",
-      },
-    },
+      } 
+    } 
   },
   variants: {
     borderWidth: ["responsive", "hover", "focus"],
-  },
-  "css.validate": false,
-  "editor.quickSuggestions": {
-    strings: true,
   },
 };
