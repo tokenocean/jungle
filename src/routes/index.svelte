1516d1871423d8aa7ddfdd533a5fc51715de7c7e
Esky33 <djescaba@icloud.com>
2023-04-29 04:23:12 +0000

    add blockstream to homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -392,19 +392,19 @@
             class="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1"
           >
             <img
-              src="https://junglelab.net/content/images/size/w2000/2023/02/logo-fs-2.png"
-              alt="Artist's & Organisers ."
+              src="https://blockstream.com/img/img/blks_logo_1920x1080.png"
+              alt="Blockstream"
               class="w-full h-full object-center object-cover"
             />
           </div>
           <h3 class="mt-6 text-sm text-gray-500">
-            <a href="https://junglelab.net/junglelab-artists/">
+            <a href="https://blockstream.com/">
               <span class="absolute inset-0" />
-              JUNGLELABᵀᴹ Artist's & Organisers
+              Blockstream
             </a>
           </h3>
           <p class="text-base font-semibold text-gray-900">
-            Create, Upload, Collect, and Transact Digital Assets on the Liquid Bitcoin Network
+            Rethinking Trust
           </p>
         </div>
 
