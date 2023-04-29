3c84813553ea5d488b9eb2e8fe0474926433394a
Esky33 <djescaba@icloud.com>
2023-04-29 03:27:24 +0000

    update sentence

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -147,7 +147,7 @@
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="max-w-2xl mx-auto py-16 sm:py-24 lg:py-32 lg:max-w-none">
       <h2 class="text-2xl font-extrabold text-gray-900">
-        Freedom is a Must. Take Yours.
+        Stay in the loop.
       </h2>
 
       <div
