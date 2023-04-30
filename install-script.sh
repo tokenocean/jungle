6b35191b0bc57642af213a4cd5d96b93e41f463f
Esky33 <djescaba@icloud.com>
2023-04-30 08:37:11 +0000

    optimized bash shell script code

--- install-script.sh
+++ install-script.sh
@@ -3,57 +3,29 @@
 # Downloads and installs the latest version of Hasura GraphQL Engine command-line interface using cURL and Bash.
 curl -L https://github.com/hasura/graphql-engine/raw/stable/cli/get.sh | bash
 
-# Uses pnpm to install the dependencies for the project.
-pnpm install
-
-# Changes the directory to the "hasura" folder in order to execute commands specific to that folder.
-cd hasura
-
-# Copies the sample environment variables file to create a new ".env" file which can be edited to include your required details.
-cp .env.sample .env
-
-# Runs a Docker image with the specified parameters to install dependencies required for the app using pnpm.
-install_dependencies() {
-    docker run -it \
+# Install dependencies inside docker container
+docker run -it \
         -v $PWD/app:/app \
         --entrypoint pnpm \
         asoltys/lnft-server \
         install
-}
-install_dependencies
 
-docker network create net
+# Create a network if it does not exists, otherwise skip
+docker network ls | grep net > /dev/null || docker network create net
 
-# Starts the Docker Compose services defined in the "docker-compose.yaml" configuration file.
-start_docker_compose() {
-    docker-compose -f docker-compose.yaml up -d && docker-compose -f docker-compose.yaml ps -q | xargs docker inspect --format '{{ .State.Status }}' | grep running | wc -l | while read COUNT ; do if [ $COUNT -eq 8 ] ; then break ; else echo "Waiting for all containers to start..." ; sleep 1 ; fi ; done
-}
-start_docker_compose
+# Start docker-compose services
+docker-compose -f docker-compose.yaml up -d && \
+wait_until_all_containers_started=$(docker-compose -f docker-compose.yaml ps -q | xargs docker inspect --format '{{ .State.Status }}' | grep running | wc -l) && \
+while [ $wait_until_all_containers_started -ne 8 ]; do echo "Waiting for all containers to start..."; sleep 1 ; done
 
-# Wait for the services to start up.
-echo "Waiting for containers to start..."
-sleep 30
-
-# Continue with the rest of the script.
-echo "All services started successfully."
-
-# Applies any pending migrations to the database schema.
-hasura migrate apply
-
-# Applies any pending metadata changes to the Hasura instance.
-hasura metadata apply
-
-# Wait for some time before applying seeds to ensure that the metadata and schema are applied correctly.
-echo "Waiting for 10 seconds before applying seed data..."
-sleep 10
-
-# Applies seed data to the database.
-hasura seeds apply
-
-# Reloads the Hasura metadata after applying changes.
+# Apply migrations, metadata, seeds and reload metadata after applying changes
+hasura migrate apply && \
+hasura metadata apply && \
+sleep 10 && \
+hasura seeds apply && \
 hasura metadata reload
 
-# Configures IPFS to use the public gateway with the specified settings.
+# Configure IPFS to use the public gateway with the specified settings.
 docker exec -it ipfs ipfs config --json Gateway.PublicGateways '{ "ipfs": { "Paths": ["/ipfs", "/ipns"], "UseSubdomains": false } }'
 
 # Configures the IPFS gateway to listen on port 8080.
@@ -72,17 +44,8 @@ docker exec -it liquid elements-cli createwallet coinos
 # Rescans the blockchain for any transactions related to the created wallet.
 docker exec -it liquid elements-cli rescanblockchain
 
-# Restarts a service named "lapp".
+# Restart a single container named "lapp"
 docker restart lapp
 
-# Changes the directory back to the previous folder.
-cd ..
-
-# Stop the container junglelab/junglelab:latest for local work
-docker stop junglelab/junglelab:latest
-
 # Uses pnpm to start the development server at http://localhost:3000.
-start_dev_server() {
-    pnpm dev
-}
-start_dev_server
+pnpm dev
