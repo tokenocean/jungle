a82afc5a8741cb67cc3d32f168ebd28cd4d50ff9
Esky33 <djescaba@icloud.com>
2023-05-01 12:14:48 +0000

    add more tags to gallery

--- src/lib/galleries.js
+++ src/lib/galleries.js
@@ -1,6 +1,16 @@
 export default {
-  bitcoin2021: "Bitcoin 2021",
   junglelab: "JungleLab",
+  ticket: "Buy Tickets",
+  event: "Events",
+  junglelab: "JungleLab",
+  membership: "Memberships",
+  art: "Art",
+  music: "Music",
+  video: "Video",
+  3d: "3D Art",
+  collectors: "Collectors",
+  games: "Games",
+  bitcoin2021: "Bitcoin 2021",
   bitcoin2023: "Bitcoin 2023",
   liquid: "liquid"
 };
