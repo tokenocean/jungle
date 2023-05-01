8a6c0542a8134faad7abf1e3ab6cbcae6525bb26
Esky33 <djescaba@icloud.com>
2023-05-01 12:59:07 +0000

    update

--- src/lib/galleries.js
+++ src/lib/galleries.js
@@ -1,18 +1,5 @@
 export default {
   junglelab: "JungleLab",
-  ticket: "Buy Tickets",
-  junglelab: "JungleLab",
-  membership: "Memberships",
-  brand: "Brand",
-  art: "Art",
-  music: "Music",
-  video: "Video",
-  3d: "3D Art",
-  photography: "Photography",
-  collectors: "Collectors",
-  games: "Games",
-  bitcoin2021: "Bitcoin 2021",
-  bitcoin2023: "Bitcoin 2023",
   liquid: "liquid"
 };
 
