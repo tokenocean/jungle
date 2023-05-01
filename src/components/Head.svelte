86be414c0b3b332a93ef4ac94f43933c0d0951d6
Esky33 <djescaba@icloud.com>
2023-05-01 02:42:26 +0100

    apple-mobile-web-app-status-bar-style default

--- src/components/Head.svelte
+++ src/components/Head.svelte
@@ -36,7 +36,7 @@
   <link rel="apple-touch-icon" sizes="180x180" href="/resources/favicons/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/resources/favicons/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="/resources/favicons/favicon-16x16.png">
-  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+  <meta name="apple-mobile-web-app-status-bar-style" content="default">
 
   {#if !dev}
     <!-- Global site tag (gtag.js) - Google Analytics -->
