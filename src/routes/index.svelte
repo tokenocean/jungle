fc1ec8223a4bfaa1c857976ac54cc70db2956d70
Esky33 <djescaba@icloud.com>
2023-06-14 01:23:43 +0000

    revert

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,8 @@
 <script context="module">
+	import { asset } from './../../lib/store.js';
+  export const prerender = true;
+</script>
+<script context="module">
 	import { assets } from './../lib/store.js';
   export async function load({ fetch }) {
     const props = await fetch(`/artworks/recent`).then((r) => r.json());
