b2638a806b7c0a2a964f0cd71131442aca5af9bc
Esky33 <djescaba@icloud.com>
2023-06-27 00:17:37 +0100

    Update [slug].svelte
    same as rare

--- src/routes/u/[slug].svelte
+++ src/routes/u/[slug].svelte
@@ -1,8 +1,9 @@
 <script context="module">
-  export async function load({ params: { slug } }) {
+  export async function load({ params: { slug }}) {
     return {
       status: 302,
       redirect: `/${slug}`,
     };
   }
+
 </script>
