3fba25347f9939d45fd488b798ede8e3d8704052
Esky33 <djescaba@icloud.com>
2023-07-16 01:43:15 +0100

    Update index.svelte
    same as rare

--- src/routes/activity/index.svelte
+++ src/routes/activity/index.svelte
@@ -1,27 +1,14 @@
 <script context="module">
   export async function load({ fetch }) {
-    try {
-      const response = await fetch(`/activity.json`);
+    const props = await fetch(`/activity.json`).then((r) =>
+      r.json()
+    );
 
-      if (!response.ok) {
-        throw new Error('Network response was not OK');
-      }
-
-      const props = await response.json();
-
-      return {
-        props,
-      };
-    } catch (error) {
-      console.error(error);
-      // Handle the error gracefully, e.g., show an error message or fallback content.
-      return {
-        props: {
-          error: error.message,
-        },
-      };
-    }
+    return {
+      props,
+    };
   }
+
 </script>
 
 <script>
@@ -32,15 +19,11 @@
 
 <div class="container mx-auto my-10 md:my-20">
   <h2>Activity</h2>
-  {#if transactions}
-    <div class="flex flex-wrap justify-between mt-10 lg:mt-20">
-      <div class="w-full lg:max-w-lg mx-auto">
-        {#each transactions as transaction}
-          <Activity {transaction} showImage={true} />
-        {/each}
-      </div>
+  <div class="flex flex-wrap justify-between mt-10 lg:mt-20">
+    <div class="w-full lg:max-w-lg mx-auto">
+      {#each transactions as transaction}
+        <Activity {transaction} showImage={true} />
+      {/each}
     </div>
-  {:else}
-    <p>No transactions available.</p>
-  {/if}
+  </div>
 </div>
