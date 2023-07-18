801e4a38d27ab55fc7d6eb861e561711bc31f7ee
Eskyee <djescaba@icloud.com>
2023-07-18 06:07:31 +0100

    fix head metadata plus add google tag for junglelab

--- src/components/Head.svelte
+++ src/components/Head.svelte
@@ -8,43 +8,41 @@
 
 <svelte:head>
   <title>{metadata.title}</title>
-  <meta property="og:title" content="{metadata.title}" />
-  <meta name="twitter:title" content="{metadata.title}" />
-  <meta name="twitter:card" content="summary" />
+  <meta property="og:title" content={metadata.title} />
+  <meta name="twitter:title" content={metadata.title} />
 
   <meta property="og:type" content="website" />
-  <meta property="og:description" content="{metadata.description}" />
-  <meta name="description" content="{metadata.description}" />
+  <meta property="og:description" content={metadata.description} />
+  <meta name="description" content={metadata.description} />
 
-  <meta name="keywords" content="{metadata.keywords}" />
+  <meta name="keywords" content={metadata.keywords} />
 
   {#if metadata.video}
-    <meta property="og:video" content="{metadata.video}" />
-    <meta name="twitter:video" content="{metadata.video}" />
+    <meta property="og:video" content={metadata.video} />
+    <meta name="twitter:video" content={metadata.video} />
     <meta property="og:video:height" content="640" />
     <meta property="og:video:width" content="385" />
     <meta name="twitter:card" content="player" />
-    <meta name="twitter:player" content="{host + $page.url.pathname}" />
+    <meta name="twitter:player" content={host + $page.url.pathname} />
   {:else if metadata.image}
-    <meta property="og:image" content="{metadata.image}" />
-    <meta name="twitter:image" content="{metadata.image}" />
+    <meta property="og:image" content={metadata.image} />
+    <meta name="twitter:image" content={metadata.image} />
     <meta name="twitter:card" content="summary_large_image" />
   {/if}
 
-  <meta property="og:url" content="{host + $page.url.pathname}" />
-  <meta name="twitter:site" content="{metadata.twitter.site}" />
-  <meta name="twitter:creator" content="{metadata.twitter.creator}" />
+  <meta property="og:url" content={host + $page.url.pathname} />
+  <meta name="twitter:site" content={metadata.twitter.site} />
+  <meta name="twitter:creator" content={metadata.twitter.creator} />
 
   {#if !dev}
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-H25E4PKPEC"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag("js", new Date());
-      gtag("config", "G-H25E4PKPEC");
-    </script>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DX592BCGN3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-DX592BCGN3');
+</script>
   {/if}
 </svelte:head>
