e97b6d09f1842e7cd35d8c527b8d344120f2ce4b
Esky33 <djescaba@icloud.com>
2023-07-19 03:45:45 +0100

    Update index.svelte
    sorted typo --

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -352,7 +352,6 @@
   </div>
 </div>
 
--->
 <div class="bg-white-100">
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="max-w-2xl mx-auto py-16 sm:py-24 lg:py-32 lg:max-w-none">
